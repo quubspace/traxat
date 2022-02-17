55c1cac0bcf868af3805ee735854a72d857b224f
Daniil Rose <daniil.rose@member.fsf.org>
2022-02-16 20:29:07 -0500

    Update deploy script and add basic TCP stream support

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,13 @@ set -o xtrace
 
 # Should only have to change these two
 readonly APP_NAME="automated-satellite-tracker"
-readonly TARGET_HOST="odessen"
+readonly TARGET_HOST="quub-pi"
 
-readonly TARGET_PATH=/home/ubuntu/$APP_NAME
+readonly TARGET_PATH="/home/pi/$APP_NAME"
 # Change depending on if running on 64-bit ARM (such as RPi 4 with Ubuntu)
 # readonly TARGET_ARCH=armv7-unknown-linux-musleabihf
 readonly TARGET_ARCH="aarch64-unknown-linux-musl"
-readonly SOURCE_PATH=./target/${TARGET_ARCH}/release/$APP_NAME
+readonly SOURCE_PATH="."
 
 if ! command -v docker &> /dev/null
 then
@@ -42,7 +42,8 @@ fi
 # TODO: Run Tests
 # cross test
 
-cross build --target=${TARGET_ARCH} --release
-rsync ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
+rsync -r ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
 
 echo "Upload successful!"
+
+TERM="xterm" ssh $TARGET_HOST "cd $TARGET_PATH && /home/pi/.cargo/bin/cargo run"
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("blank.");
+use anyhow::Result;
+use tokio::io::AsyncReadExt;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let rotctld_port = "4533";
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", rotctld_port)).await?;
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        println!("Socket now listening.");
+
+        tokio::spawn(async move {
+            let mut buf = [0; 4096];
+
+            // In a loop, read data from the socket and print the data back.
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("Failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+
+                // Print data
+                println!("{:?}", buf);
+            }
+        });
+    }
 }
